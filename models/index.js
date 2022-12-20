a97effe36691bb1329f76801ece7bffe5592a296
lynnga <lingamliel@gmail.com>
2022-12-19 22:53:39 -0600

    fixed bug

--- models/index.js
+++ models/index.js
@@ -1,35 +1,33 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-  onDelete:'CASCADE',
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id',
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   // Define the third table needed to store the foreign keys
-  through: { ProductTag,
-    foreignKey: 'product_id'
-  },
+  through: ProductTag,
+  foreignKey: "product_id",
 });
-
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   // Define the third table needed to store the foreign keys
   through: ProductTag,
-  foreignKey: 'tag_id'
-  });
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -122,7 +122,20 @@ router.put("/:id", (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
-  product.destroy();
+  Product.destroy({
+ where: {
+      id: req.params.id,
+    },
+  })
+  
+  .then((data) => res.status(200).json(data))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
+
+
+  
   // delete one product by its `id` value
 });
 
